c8f55b6947cdd4d07a52724a47300db18e3b9fdc
jzlbornoz <javieraap10@gmail.com>
2023-05-09 10:53:27 -0400

    [image model]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,33 @@ import { Inter } from "next/font/google";
 import { NextPage } from "next";
 import { RandomFoxes } from "@/components/RandomFox";
 import { useState } from "react";
+import { ImageModel } from "@/models/Image.model";
 
 const inter = Inter({ subsets: ["latin"] });
 
+//return a number between 1 and 122
+const randomNumber = () => Math.floor(Math.random() * 121) + 1;
+
+// return id
+const uniqueId = () => Math.random().toString(36).substring(2, 9);
 const Home: NextPage = () => {
-  //return a number between 1 and 122
-  const randomNumber = () => Math.floor(Math.random() * 121) + 1;
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
+  const [images, setImages] = useState<ImageModel[]>([
+    {
+      id: uniqueId(),
+      url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    },
+    {
+      id: uniqueId(),
+      url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    },
+    {
+      id: uniqueId(),
+      url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    },
+    {
+      id: uniqueId(),
+      url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
+    },
   ]);
 
   //--
@@ -21,8 +37,8 @@ const Home: NextPage = () => {
     <main>
       <h2 className="text-4xl font-bold text-gray-900">Random Foxes</h2>
       {images.map((item, index) => (
-        <div key={index} className="p-4">
-          <RandomFoxes url={item} />
+        <div key={item.id + index} className="p-4">
+          <RandomFoxes url={item.url} />
         </div>
       ))}
     </main>
