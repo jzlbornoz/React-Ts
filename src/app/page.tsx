d25cb5db99e47d27db27609a8abe3c30cfcaa940
jzlbornoz <javieraap10@gmail.com>
2023-05-10 11:47:11 -0400

    [tipado de eventos]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { Inter } from "next/font/google";
+import { MouseEventHandler, useState } from "react";
 import { NextPage } from "next";
+import { Inter } from "next/font/google";
+
 import { RandomFoxes } from "@/components/RandomFox";
-import { useState } from "react";
 import { ImageModel } from "@/models/Image.model";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,29 +14,30 @@ const randomNumber = () => Math.floor(Math.random() * 121) + 1;
 // return id
 const uniqueId = () => Math.random().toString(36).substring(2, 9);
 const Home: NextPage = () => {
-  const [images, setImages] = useState<ImageModel[]>([
-    {
+  const [images, setImages] = useState<ImageModel[]>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault(); //Ejemplo
+
+    const newImage: ImageModel = {
       id: uniqueId(),
       url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    },
-    {
-      id: uniqueId(),
-      url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    },
-    {
-      id: uniqueId(),
-      url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    },
-    {
-      id: uniqueId(),
-      url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    },
-  ]);
+    };
+    setImages([...images, newImage]);
+  };
 
   //--
   return (
     <main>
       <h2 className="text-4xl font-bold text-gray-900">Random Foxes</h2>
+      <button
+        onClick={addNewFox}
+        className="flex  justify-center rounded-md bg-indigo-600 
+      px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 
+      focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Add Fox
+      </button>
       {images.map((item, index) => (
         <div key={item.id + index} className="p-4">
           <RandomFoxes url={item.url} />
