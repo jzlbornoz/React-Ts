a0a0d699819bc651d6ace4b83ba7199b9e302c94
jzlbornoz <javieraap10@gmail.com>
2023-05-13 11:46:10 -0400

    [lazy loading con observadores]

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 type Props = {
   url: string;
@@ -7,15 +7,37 @@ type Props = {
 
 const RandomFoxes = ({ url }: Props): JSX.Element => {
   const node = useRef<HTMLImageElement>(null); // Con el null se evitan conflictos con TS y React
+  const emptyImage = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  const [src, setSrc] = useState<string>(emptyImage);
+
+  useEffect(() => {
+    //new observer
+    const observer = new IntersectionObserver((entries) => entries.forEach((entry) => {
+      //On intersection
+      entry.isIntersecting && setSrc(url);
+    }));
+
+
+
+
+    //observe
+    if (node.current) {
+      observer.observe(node.current);
+    }
+    //desconect
+    return () => {
+      observer.disconnect();
+    }
+  }, [url]);
 
   return (
     <Image
       ref={node}
-      src={url}
+      src={src}
       width={350}
       height={350}
       alt="Fox"
-      className="rounded"
+      className="rounded bg-slate-600"
     />
   );
 };
